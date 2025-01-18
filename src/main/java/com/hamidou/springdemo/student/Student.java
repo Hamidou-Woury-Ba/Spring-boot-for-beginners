2798f9b11794db4b60bb0f086a8c57d8c19bc31d
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2025-01-18 18:14:32 +0000

    Implementing service layer

---
+++ src/main/java/com/hamidou/springdemo/student/Student.java
@@ -0,0 +1,64 @@
+package com.hamidou.springdemo.student;
+
+public class Student {
+    private String firstname;
+
+    private String lastname;
+
+    private String dateOfBirth;
+
+    private String email;
+
+    private int age;
+
+    public Student(String firstname, String lastname, String dateOfBirth, String email, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String dateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
