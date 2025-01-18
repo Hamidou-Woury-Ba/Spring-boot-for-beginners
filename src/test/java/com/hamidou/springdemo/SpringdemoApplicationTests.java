d3ac3dc725a126bb8f3ee8b1aee7f3e7fd5ccfe3
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2025-01-18 15:14:29 +0000

    Initializing spring boot project

---
+++ src/test/java/com/hamidou/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamidou.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
